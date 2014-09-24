ad4d3210e36700af6c599e8c65f7da2bb4332268
Joshua Tucker <j.allentucker@gmail.com>
2014-09-23 17:14:43 -0700

    eliminated some magic numbers

--- pay-me-daily.js
+++ pay-me-daily.js
@@ -2,7 +2,12 @@ var fs = require('fs');
 var _ = require('lodash');
 
 var filesRead = 0;
-var fanduelArray, qbs, rbs, wrs, tes, ks;
+var qbProjectionColumn = 15;
+var rbProjectionColumn = 14;
+var wrProjectionColumn = 14;
+var teProjectionColumn = 14;
+var kProjectionColumn = 16;
+var fanduelArray, qbs, rbs, wrs, tes, ks, ds;
 
 var makeFanduelArray = function(dataString) {
 	var json = JSON.parse(dataString);
@@ -12,8 +17,8 @@ var makeFanduelArray = function(dataString) {
 	return fanduelArray;
 };
 
-var preparePlayersArray = function(dataString, column) {
-	var playersArray = makePlayersArray(dataString, column);
+var preparePlayersArray = function(dataString, projectionColumn) {
+	var playersArray = makePlayersArray(dataString, projectionColumn);
 	playersArray = addSalaries(playersArray);
 	playersArray = trimPlayersArray(playersArray);
 	playersArray.sort(function(a, b) {
@@ -22,23 +27,14 @@ var preparePlayersArray = function(dataString, column) {
 	return playersArray;
 };
 
-var makePlayersArray = function(dataString, column) {
+var makePlayersArray = function(dataString, projectionColumn) {
 	var playerStrings = dataString.split('\r\n');
 	filesRead++;
 	return playerStrings.map(function(playerString) {
 		var playerArray = playerString.split('\t');
-		return [playerArray[0].split('(')[0].trim(), parseFloat(playerArray[column - 1])];
+		return [playerArray[0].split('(')[0].trim(), parseFloat(playerArray[projectionColumn - 1])];
 	});
 };
-
-// var makeDsArray = function(dataString) {
-// 	var playerStrings = dataString.split('\r\n');
-// 	filesRead++;
-// 	return playerStrings.map(function(playerString) {
-// 		var playerArray = playerString.split('\t');
-// 		return [playerArray[0].split('(')[0].trim(), parseFloat(playerArray[11])];
-// 	});
-// };
 
 var addSalaries = function(playersArray) {
 	return playersArray.map(function(playerArray) {
@@ -70,23 +66,23 @@ var pickTeam = function() {
 fs.readFile('fanduel.txt', { encoding: 'utf8' }, function(err, fanduelData) {
 	fanduelArray = makeFanduelArray(fanduelData);
 	fs.readFile('qbs.txt', { encoding: 'utf8' }, function(err, qbData) {
-		qbs = preparePlayersArray(qbData, 15);
+		qbs = preparePlayersArray(qbData, qbProjectionColumn);
 		console.log(qbs);
 	});
 	fs.readFile('rbs.txt', { encoding: 'utf8' }, function(err, rbData) {
-		rbs = preparePlayersArray(rbData, 14);
+		rbs = preparePlayersArray(rbData, rbProjectionColumn);
 		console.log(rbs);
 	});
 	fs.readFile('wrs.txt', { encoding: 'utf8' }, function(err, wrData) {
-		wrs = preparePlayersArray(wrData, 14);
+		wrs = preparePlayersArray(wrData, wrProjectionColumn);
 		console.log(wrs);
 	});
 	fs.readFile('tes.txt', { encoding: 'utf8' }, function(err, teData) {
-		tes = preparePlayersArray(teData, 14);
+		tes = preparePlayersArray(teData, teProjectionColumn);
 		console.log(tes);
 	});
 	fs.readFile('ks.txt', { encoding: 'utf8' }, function(err, kData) {
-		ks = preparePlayersArray(kData, 16);
+		ks = preparePlayersArray(kData, kProjectionColumn);
 		console.log(ks);
 	});
 	// fs.readFile('ds.txt', { encoding: 'utf8' }, function(err, dData) {
