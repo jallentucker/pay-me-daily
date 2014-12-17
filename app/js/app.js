9ed0a7e2ef27c78e3b4b11365c3493813eb7369e
Joshua Tucker <j.allentucker@gmail.com>
2014-12-17 09:37:50 -0800

    added some players to display on home page

--- app/js/app.js
+++ app/js/app.js
@@ -4,3 +4,26 @@ App.Router.map(function() {
 	this.route("projections");
 	this.route("about");
 });
+
+App.IndexRoute = Ember.Route.extend({
+	model: function() {
+		return [
+			{
+				position: "QB",
+				name: "Matt Ryan",
+				projection: 22.88,
+				salary: 7900
+			}, {
+				position: "RB",
+				name: "Le\'veon Bell",
+				projection: 22.88,
+				salary: 9600
+			}, {
+				position: "RB",
+				name: "Jamaal Charles",
+				projection: 19.98,
+				salary: 9200
+			}
+		];
+	}
+});
